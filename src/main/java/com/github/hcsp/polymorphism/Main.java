9b9e56f436f7167d91c3c542e6585e78378f9951
zhangyongwang <20225614@qq.com>
2019-07-03 12:42:06 +0800

    使用接口的默认方法实现mixin

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -1,45 +1,54 @@
 package com.github.hcsp.polymorphism;
 
 public class Main {
-    abstract static class 会跑 {
+    interface 会跑 {
+        void 跑();
+    }
+
+    interface 会游泳 {
+        void 游泳();
+    }
+
+    abstract static class 能跑 implements 会跑 {
         String name;
 
-        会跑(String name) {
+        能跑(String name) {
             this.name = name;
         }
 
-        String getName() {
+        public String getName() {
             return name;
         }
 
-        void 跑() {
-            System.out.println(name + "跑啊跑啊跑");
+        public void 跑() {
+            System.out.println("跑啊跑啊跑");
         }
     }
 
-    abstract static class 会游泳 {
-        String name;
-
-        会游泳(String name) {
-            this.name = name;
-        }
-
-        String getName() {
-            return name;
-        }
-
-        void 游泳() {
-            System.out.println(name + "游啊游啊游");
-        }
-    }
-
-    static class 猫 extends 会跑 {
+    static class 猫 extends 能跑 {
         猫() {
             super("小花猫");
         }
     }
 
-    static class 鱼 extends 会游泳 {
+
+    abstract static class 能游泳 implements 会游泳 {
+        String name;
+
+        public 能游泳(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void 游泳() {
+            System.out.println("游啊游啊游");
+        }
+    }
+
+    static class 鱼 extends 能游泳 {
         鱼() {
             super("鱼");
         }
@@ -49,8 +58,17 @@ public class Main {
     // 请尝试通过接口的默认方法实现mixin
     // 从而实现最大程度的代码复用
     //
-    // static class 乌龟 extends 会跑, 会游泳 {
-    // }
+    static class 乌龟 implements 会跑, 会游泳 {
+        @Override
+        public void 跑() {
+            System.out.println("乌龟跑啊跑啊跑");
+        }
+
+        @Override
+        public void 游泳() {
+            System.out.println("乌龟游啊游啊游");
+        }
+    }
 
     public static void main(String[] args) {
         new 乌龟().跑();
